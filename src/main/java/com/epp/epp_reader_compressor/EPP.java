97e171cc0446fa072e807baee77a50c8987849a0
Szkolarz <102251691+Szkolarz@users.noreply.github.com>
2022-08-31 07:56:49 +0200

    test

--- src/main/java/com/epp/epp_reader_compressor/EPP.java
+++ src/main/java/com/epp/epp_reader_compressor/EPP.java
@@ -12,7 +12,7 @@ public class EPP extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EPP.class.getResource("epp-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("EPP!");
+        stage.setTitle("EPP");
         stage.setScene(scene);
         stage.show();
     }
