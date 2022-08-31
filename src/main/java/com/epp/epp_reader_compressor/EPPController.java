54cbd55d88d1149cffaa2699235969418601fba2
Szkolarz <102251691+Szkolarz@users.noreply.github.com>
2022-08-31 08:05:13 +0200

    Initial .epp read

--- src/main/java/com/epp/epp_reader_compressor/EPPController.java
+++ src/main/java/com/epp/epp_reader_compressor/EPPController.java
@@ -3,12 +3,26 @@ package com.epp.epp_reader_compressor;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.io.*;
+
 public class EPPController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onHelloButtonClick() throws IOException {
+
         welcomeText.setText("EPP Converter");
+
+        File file = new File(
+                "C:\\Users\\tholv\\Desktop\\epp\\przesyl7.22.epp");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+
+        while ((st = br.readLine()) != null)
+            System.out.println(st);
     }
-}
+
+    }
