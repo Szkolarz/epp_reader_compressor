2cd39c51d996f45253df0b88d7103dd8fcae3ecd
Szkolarz <102251691+Szkolarz@users.noreply.github.com>
2022-08-31 14:33:31 +0200

    Exporting data to txt file with polish characters and regex

--- src/main/java/com/epp/epp_reader_compressor/EPPController.java
+++ src/main/java/com/epp/epp_reader_compressor/EPPController.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -11,10 +14,14 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.nio.charset.StandardCharsets;
 
 public class EPPController {
     @FXML
     private Label welcomeText;
+
+
+    List<String> list = new ArrayList<String>();
 
     @FXML
     protected void onHelloButtonClick() throws IOException {
@@ -24,7 +31,11 @@ public class EPPController {
         File file = new File(
                 "C:\\Users\\tholv\\Desktop\\epp\\przesyl7.22.epp");
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "ISO-8859-2"));
+
+
 
 
 
@@ -34,27 +45,71 @@ public class EPPController {
         String st;
 
 
-        List<String> list = new ArrayList<String>();
+        try {
+            File myObj = new File("test.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
+
+
+
 
         while ((st = br.readLine()) != null) {
-            System.out.println(st);
+            //System.out.println(st);
             list.add(st);
         }
+        //System.out.println(list.get(1));
 
+
+        FileWriter fw = new FileWriter("C:\\Users\\tholv\\Desktop\\epp\\test.txt", true);
+        BufferedWriter bw = new BufferedWriter(fw);
         for (int i=0; i<list.size(); i++)
         {
-            String input = list.get(i);   //input string
-            String firstFourChars = "";   //substring containing first 4 characters
+            //String input = list.get(i);   //input string
+            //String firstFourChars = "";   //substring containing first 4 characters
 
-            if (input.length() > 4)
-                firstFourChars = input.substring(0, 4);
+            /*if (input.length() > 4)
+                firstFourChars = input.substring(0, 4);*/
 
-            if (firstFourChars.contains("KW"))
-            {
-                String temp = list.get(i);
-                System.out.println(temp.replaceAll("(,)\\1{1,}",","));
+            /*if (firstFourChars.contains("KW"))*/
+
+            for (int j=0; j<listOfErrors.size(); j++) {
+                if (list.get(i).contains(listOfErrors.get(j))) {
+                    String temp = list.get(i);
+                    temp = (temp.replaceAll("(,)\\1{1,}", "\t"));
+                    temp = (temp.replaceAll("[ ]*,[ ]*+", "\t"));
+                    temp = (temp.replaceAll("[\"]", " "));
+                    //System.out.println(temp.replaceAll("[ ]*,[ ]*|[ ]+", "\t"));
+                    bw.write(temp);
+                    bw.newLine();
+
+
+                    break;
+                }
             }
         }
+        bw.close();
+
+       /* Path source = Paths.get("C:\\Users\\tholv\\Desktop\\epp\\test.txt");
+        try{
+            // rename a file in the same directory
+            Files.move(source, source.resolveSibling("text.xlsx"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+
+
+System.out.println("finito");
 
     }
 
@@ -95,9 +150,9 @@ public class EPPController {
             inputStream.close();
 
 
-            for (int i=0; i<listOfErrors.size(); i++) {
+           /* for (int i=0; i<listOfErrors.size(); i++) {
                 System.out.println(listOfErrors.get(i));
-            }
+            }*/
 
         }
         catch(FileNotFoundException e)
@@ -116,52 +171,6 @@ public class EPPController {
     }
 
 
-
-   /* public static void readExcel(String filePath) {
-        File file = new File(filePath);
-        try {
-            FileInputStream inputStream = new FileInputStream(file);
-
-            Workbook baeuldungWorkBook = new HSSFWorkbook(inputStream);
-            for (Sheet sheet : baeuldungWorkBook) {
-
-                int firstRow = sheet.getFirstRowNum();
-                int lastRow = sheet.getLastRowNum();
-                for (int index = firstRow + 1; index <= lastRow; index++) {
-                    Row row = sheet.getRow(index);
-
-                    for (int cellIndex = row.getFirstCellNum(); cellIndex < row.getLastCellNum(); cellIndex++) {
-                        Cell cell = row.getCell(cellIndex, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-                        printCellValue(cell);
-                    }
-                }
-
-            }
-
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public static void printCellValue(Cell cell) {
-        CellType cellType = cell.getCellType().equals(CellType.FORMULA)
-                ? cell.getCachedFormulaResultType() : cell.getCellType();
-        if (cellType.equals(CellType.STRING)) {
-            System.out.print(cell.getStringCellValue() + " | ");
-        }
-        if (cellType.equals(CellType.NUMERIC)) {
-            if (DateUtil.isCellDateFormatted(cell)) {
-                System.out.print(cell.getDateCellValue() + " | ");
-            } else {
-                System.out.print(cell.getNumericCellValue() + " | ");
-            }
-        }
-        if (cellType.equals(CellType.BOOLEAN)) {
-            System.out.print(cell.getBooleanCellValue() + " | ");
-        }
-    }*/
 
     }
 
